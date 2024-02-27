816117d7e100ba3c579947a4edf79af0ad76f58f
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-02-28 03:18:46 +0500

    "first-JS"

---
+++ JS-first/app.js
@@ -0,0 +1,9 @@
+// Question No :04
+
+var age = 22;
+alert("I am "+ age +" years old");
+var times = 14;
+alert("You have visited this site " +times+" times");
+var birthYear = 2001;
+alert("My birth year is " +birthYear);
+alert("Data type of my declared variable is "+typeof(birthYear));
