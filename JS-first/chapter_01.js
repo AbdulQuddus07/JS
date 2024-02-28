409fe0dc64fcd849eedd167c301c40b233c684de
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-02-28 23:48:51 +0500

    chapter_01

--- JS-first/chapter_01.js
+++ JS-first/chapter_01.js
@@ -1,2 +1,14 @@
 // Chapter No :01
+
+// 2. Write a script to display following message on your web 
 alert("Error! Please enter a valid password");
+
+
+// 3. Write a script to display following message on your web 
+//  page: (Hint : Use line break)
+alert("Wellcome to JS Land... \nHappy Coding!");
+
+
+// Generate the following message through browser’s 
+// developer console
+console.log("Hi I can run Java Script through my web browser console");
