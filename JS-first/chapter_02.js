16d2fde58ea3e95c7ee3bc883e98cc16e710afc3
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-02-29 00:02:19 +0500

    'chapter_02_done'

---
+++ JS-first/chapter_02.js
@@ -0,0 +1,18 @@
+// 1. Declare a variable called username.
+
+var username;
+
+// 2. Declare a variable called myName & assign to it a string.
+// that represents your Full Name.
+var myName;
+myName = "Abdul Quddus";
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// 4. Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes.
+var title_message;
+title_message = "Hi World!"
+alert(title_message);
