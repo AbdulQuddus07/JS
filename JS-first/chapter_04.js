da719432409c2a0f5c82f9dc7e0d78c08ee37c2d
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-02-29 11:22:18 +0500

    "work done"

--- JS-first/chapter_04.js
+++
@@ -1,8 +0,0 @@
-// Chapter No :04
-var age = 22;
-alert("I am "+ age +" years old");
-var times = 14;
-alert("You have visited this site " +times+" times");
-var birthYear = 2001;
-alert("My birth year is " +birthYear);
-alert("Data type of my declared variable is "+typeof(birthYear));
