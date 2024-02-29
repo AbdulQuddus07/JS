9a847cbc667e5f5706565a8fa2e7934451790e5e
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-02-29 11:15:54 +0500

    "chapter_02_done"

--- JS-first/chapter_02.js
+++ JS-first/chapter_02.js
@@ -16,3 +16,40 @@ myName = "Abdul Quddus";
 var title_message;
 title_message = "Hi World!"
 alert(title_message);
+
+// 4. Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes.
+username="Jhone Doe";
+var age=15;
+var course = "Certified Mobile Application Development"
+alert(username);
+alert(age+" yearsold");
+alert(course);
+
+// 5. Write a script to display the following alert using one JS 
+// variable:
+var value = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(value);
+
+// 6.6. Declare a variable called email and assign to it a string that 
+// represents your Email Address(e.g. example@example.com). 
+// Show the blow mentioned message in an alert box.(Hint: use 
+// string concatenation)
+var email = "example@example.com";
+alert("My email address is "+email);
+
+
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+var book = "A smarter\n way to learn JavaScript"
+alert("I am trying to learn from the Book "+book);
+
+// 8. Write a script to display this in browser through JS
+document.write("Yes i can write content through JS");
+
+// 9. Store following string in a variable and show in alert and 
+// browser through JS
+var string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(string);
