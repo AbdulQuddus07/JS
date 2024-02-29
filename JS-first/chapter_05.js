fbbe506879635c672c174bf7212342a80cce42cd
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-02-29 12:44:27 +0500

    "chapter_03_Done"

---
+++ JS-first/chapter_05.js
@@ -0,0 +1,11 @@
+// 01:1. Write a program that take two numbers & add them in a new variable. Show the result in your browser
+var num1 = 3;
+var num2 = 5;
+var result1 = num1+num2;
+var result2 = num1-num2;
+var result3 = num1*num2;
+var result4 = num1/num2;
+document.write("Addition of "+num1+" and " +num2+ " is " +result1+ "<br>");
+document.write("Subtraction of "+num1+" from " +num2+ " is " +result2+ "<br>");
+document.write("Multiplication of "+num1+" from " +num2+ " is " +result3 + "<br>");
+document.write("Division of "+num1+" and " +num2+ " is " +result4+ "<br>");
